8ed9e1a182505950fa90e23899affe0b5553a379
kachemirova <kachemirovaelena@gmail.com>
2019-09-24 16:43:38 +0300

    Add not_supported text

--- src/lang/locales/ru.js
+++ src/lang/locales/ru.js
@@ -2,6 +2,7 @@ export default function ru() {
     return {
         translation: {
             oops: 'Упс, что-то\nсломалось',
+			not_supported: 'Сервис не поддерживается Вашим устройством',
 			cancel: 'Отмена',
 			error_details: 'Подробнее об ошибке',
 			network: 'Проверьте соединение с интернетом',
