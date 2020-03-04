8aff99d150b20018d1a675b08c5ba28ad89e9ab8
Иван Недзвецкий <stels-cs@yandex.ru>
2020-03-04 14:41:14 +0300

    Best code for desktop container

--- src/containers/DesktopContainer/DesktopContainer.js
+++ src/containers/DesktopContainer/DesktopContainer.js
@@ -5,7 +5,7 @@ import FatalErrorDesktop from "../../components/FatalErrorDesktop/FatalErrorDesk
 import {Route} from "../../routing/Route"
 import {PAGE_ENTITY, PAGE_MAIN, MODAL_MAIN} from "../../routing/routes"
 import {popPage, pushModal, pushPage} from "../../routing/methods"
-import {Button} from "@happysanta/vk-app-ui"
+import {Button,PageRoot} from "@happysanta/vk-app-ui"
 import PopupDesktop from "../../components/PopupDesktop/PopupDesktop"
 import '@happysanta/vk-app-ui/dist/vkappui.css'
 
@@ -14,58 +14,36 @@ export const DEFAULT_SCROLL_SPEED = 1500
 
 class DesktopContainer extends Component {
 
-	goBack() {
-		popPage()
-	}
-
-	renderPopup(route) {
-		switch (route.getActiveModal()) {
-			case MODAL_MAIN:
-				return <PopupDesktop showCross={true} onClick={() => this.goBack()} onClose={() => this.goBack()}>
-					<div style={{background: '#FFF', minHeight: 200, padding: 16}}>
-						Попап
-					</div>
-				</PopupDesktop>
-			default:
-				return null
-		}
-	}
-
-	renderPage(route) {
-		switch (route.getPageId()) {
-			case PAGE_MAIN:
-				return <div>
-					Главная страница
-					<div style={{textAlign: 'center', paddingTop: 10}}>
-						<Button onClick={() => pushPage(PAGE_ENTITY, {entityId: 0})}>
-							На страницу сущности
-						</Button>
-					</div>
-				</div>
-			case PAGE_ENTITY:
-				return <div>
-					Сущность
-					<div style={{textAlign: 'center', paddingTop: 10}}>
-						<Button onClick={() => pushModal(MODAL_MAIN)}>
-							Открыть попап
-						</Button>
-					</div>
-				</div>
-			default:
-				return null
-		}
-	}
-
 	render() {
 		let {fatal, location} = this.props
 		if (fatal) {
 			return <FatalErrorDesktop error={fatal} onClose={getOnErrorClose(fatal, () => this.props.removeFatalError())}/>
 		}
 		let route = Route.fromLocation(location.pathname, location.state, location.search)
-		return <div>
-			{this.renderPage(route)}
-			{this.renderPopup(route)}
-		</div>
+		return <PageRoot popup={<PageRoot activePage={route.getActiveModal()}>
+			<PopupDesktop showCross={true} id={MODAL_MAIN} onClick={popPage} onClose={popPage}>
+				<div style={{background: '#FFF', minHeight: 200, padding: 16}}>
+					Попап
+				</div>
+			</PopupDesktop>
+		</PageRoot>} activePage={route.getPageId()}>
+			<div id={PAGE_MAIN}>
+				Главная страница
+				<div style={{textAlign: 'center', paddingTop: 10}}>
+					<Button onClick={() => pushPage(PAGE_ENTITY, {entityId: 0})}>
+						На страницу сущности
+					</Button>
+				</div>
+			</div>
+			<div id={PAGE_ENTITY}>
+				Сущность
+				<div style={{textAlign: 'center', paddingTop: 10}}>
+					<Button onClick={() => pushModal(MODAL_MAIN)}>
+						Открыть попап
+					</Button>
+				</div>
+			</div>
+		</PageRoot>
 	}
 }
 
